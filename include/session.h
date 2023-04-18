dabbb431fe3ee0bc3201534b52a4745bd57bb3a6
Rowan McKereghan <romckereghan@g.ucla.edu>
2023-04-18 04:33:58 +0000

    HTTP response message now precedes echoed response body
    also, pushed changes for working echoing server onto main instead of testbranch and client terminates after newlines in HTTP request are found
    
    Change-Id: If0460695261c109555a2b481cbd7a9ff5d362ed5

--- include/session.h
+++ include/session.h
@@ -5,6 +5,8 @@
 #include <boost/bind.hpp>
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <time.h>
 
 // session for handling async reads and writes through a socket
 
@@ -21,9 +23,19 @@ class session {
 
   void handle_write(const boost::system::error_code& error);
 
+  void handleHTTPwrite(const boost::system::error_code& error);
+
+
   boost::asio::ip::tcp::socket socket_;
   enum { max_length = 1024 };
+  //char full_request[8192]; //general max length for webservers, for later when we need a body
   char data_[max_length];
+  char date[100];
+  time_t t;
+  struct tm *myTime;
+  bool end_of_request = false;
+  std::string HTTPResponse; 
+
 };
 
 #endif
