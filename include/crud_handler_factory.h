be300f472a65191c3d4c7e9e823d181e9c2e37e1
connord2838 <connord2838@g.ucla.edu>
2023-05-19 17:58:43 -0700

    Added to crud_handler_factory.h/.cc and crud_handler.h/.cc and added some tests  - For the factory filled in GetCompleteFilePath and GetRoot     - these are the same as the methods of the same name in the static factory so maybe they could be moved somewhere so there isn't duplicate code     - Added tests for these methods in tests/factories/crud_handler_factory_tests.cc  - For the handler itself added placeholder functions to handle each type of supported HTTP methods  - Also added code in HandleRequest to parse the method and call the right method handler     - Added a test for an unsupported method, testing for the method handler functions will be added later
    Change-Id: Ib2c73cb5a9d257e37c50b2ba2672b2905b8b5041

--- include/crud_handler_factory.h
+++ include/crud_handler_factory.h
@@ -1,6 +1,8 @@
 #ifndef CRUD_HANDLER_FACTORY_H
 #define CRUD_HANDLER_FACTORY_H
+#include <list>
 #include <string>
+#include <unordered_map>
 
 #include "I_request_handler_factory.h"
 #include "config_parser.h"
@@ -10,10 +12,13 @@ class CrudHandlerFactory : public I_RequestHandlerFactory {
  public:
   CrudHandlerFactory(NginxConfig* config);
   CrudHandler* CreateHandler(std::string& uri_path);
+  // These are the same as the methods from the static handler factory
+  // Could maybe make them static methods in the base class and change GetRoot to take a config object
   std::string GetCompleteFilePath(std::string& file_path);
   std::string GetRoot();
 
  private:
   NginxConfig* config_;
+  unordered_map<std::string, std::list<int>> entity_to_id_;  // map of entity name to list of IDs
 };
 #endif
---
+++ tests/factories/crud_handler_factory_test.cc
@@ -0,0 +1,27 @@
+#include "crud_handler_factory.h"
+
+#include <string>
+
+#include "config_parser.h"
+#include "gtest/gtest.h"
+
+// Same as static handler factory tests cause these methods are the same between the classes
+
+TEST(CrudHandlerFactory, TestGetRoot) {
+  NginxConfig config;
+  std::shared_ptr<NginxConfigStatement> statement = std::make_shared<NginxConfigStatement>();
+  statement->tokens_ = {"root", "tests/crud_test_files"};
+  config.statements_ = {statement};
+  CrudHandlerFactory crud_handler_factory(&config);
+  EXPECT_EQ(crud_handler_factory.GetRoot(), "tests/crud_test_files/");
+}
+
+TEST(CrudHandlerFactory, TestGetCompleteFilePath) {
+  NginxConfig config;
+  std::shared_ptr<NginxConfigStatement> statement = std::make_shared<NginxConfigStatement>();
+  statement->tokens_ = {"root", "tests/crud_test_files"};
+  config.statements_ = {statement};
+  CrudHandlerFactory crud_handler_factory(&config);
+  std::string file_path = "index.html";
+  EXPECT_EQ(crud_handler_factory.GetCompleteFilePath(file_path), "tests/crud_test_files/index.html");
+}
