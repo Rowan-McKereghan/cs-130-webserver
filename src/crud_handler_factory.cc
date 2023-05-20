3b3673b27786a10344f4208ec498542dfdcb2632
connord2838 <connord2838@g.ucla.edu>
2023-05-19 17:58:43 -0700

    Added to crud_handler_factory.h/.cc and crud_handler.h/.cc and added some tests  - For the factory filled in GetCompleteFilePath and GetRoot     - these are the same as the methods of the same name in the static factory so maybe they could be moved somewhere so there isn't duplicate code     - Added tests for these methods in tests/factories/crud_handler_factory_tests.cc  - For the handler itself added placeholder functions to handle each type of supported HTTP methods  - Also added code in HandleRequest to parse the method and call the right method handler     - Added a test for an unsupported method, testing for the method handler functions will be added later
    Change-Id: Ib2c73cb5a9d257e37c50b2ba2672b2905b8b5041

--- src/crud_handler_factory.cc
+++ src/crud_handler_factory.cc
@@ -4,8 +4,28 @@
 
 CrudHandlerFactory::CrudHandlerFactory(NginxConfig* config) : config_(config) {}
 
-std::string CrudHandlerFactory::GetRoot() { return ""; }
+std::string CrudHandlerFactory::GetRoot() {
+  if (config_->statements_.size() == 1) {
+    std::vector<std::string> tokens = config_->statements_[0]->tokens_;
+    // looking for this format:
+    // root /serving_directory
+    if (tokens.size() >= 2 && tokens[0] == "root") {
+      std::string root = tokens[1];
+      // add back trailing slash if necessary
+      if (root.empty() || root[root.size() - 1] != '/') {
+        root += '/';
+      }
+      return root;
+    }
+  }
+  return "";
+}
 
-std::string CrudHandlerFactory::GetCompleteFilePath(std::string& file_path) { return ""; }
+std::string CrudHandlerFactory::GetCompleteFilePath(std::string& file_path) {
+  std::string root = GetRoot();
+  return root + file_path;
+}
 
-CrudHandler* CrudHandlerFactory::CreateHandler(std::string& file_path) { return new CrudHandler(file_path); }
+CrudHandler* CrudHandlerFactory::CreateHandler(std::string& file_path) {
+  return new CrudHandler(file_path, entity_to_id_);
+}
