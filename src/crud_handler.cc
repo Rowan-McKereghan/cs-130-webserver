c6f36c971e3d98a05295cb696ae6a0ee85fc51ad
ryan <ryand3031@g.ucla.edu>
2023-05-22 00:27:59 -0700

    Add implementations for Get and Delete requests - Add implementation for delete request for deleting instances of an entity 	Added a basic test for delete request - Add implementation for Get request 	- Handle Get request with ID for retrieving the information from a specific entity file 	- Handle Get request without ID for retrieving list of files in a given entity directory 		- returns empty list if entity does not exists 	- Added basic tests for Get functionality
    Change-Id: I3797ac1125e2393e9e1da30572e301e52a94212f

--- src/crud_handler.cc
+++ src/crud_handler.cc
@@ -23,6 +23,7 @@ void CrudHandler::ReturnError(StatusCode status_code, http::response<http::dynam
     status_string += "Unknown Status Code";
     LOG(warning) << "Unknown Status Code Found: " << std::to_string(status_code);
   }
+  status_string += "\n\n";
   boost::beast::ostream(res.body()) << status_string;
   res.prepare_payload();
   res.set(boost::beast::http::field::content_type, "text/HTML");
@@ -126,9 +127,95 @@ StatusCode CrudHandler::HandlePut(const http::request<http::string_body> req, ht
   return OK; 
 }
 
-// Placeholders for now
-StatusCode CrudHandler::HandleGet(http::response<http::dynamic_body>& res) { return OK; }
-StatusCode CrudHandler::HandleDelete(http::response<http::dynamic_body>& res) { return OK; }
+StatusCode CrudHandler::HandleDelete(http::response<http::dynamic_body>& res) { 
+  std::pair<std::string, int> entity = ParseTarget();
+  //Return bad request if id is included in DELETE request
+  if(entity.second==-1){
+    LOG(warning) << "entity id not specified for DELETE request";
+    return BAD_REQUEST;
+  }
+
+  int id = entity.second;
+
+  //check if entity with given id does not exist return bad request error
+  //As mentioned above may in the future find more descriptive error to give
+  if(entity_to_id_->find(entity.first)==entity_to_id_->end() || (*entity_to_id_)[entity.first].find(id)== (*entity_to_id_)[entity.first].end()){
+    LOG(warning) << "Entity: " << entity.first << " not found with id " << id;
+    return BAD_REQUEST;
+  }
+
+  boost::filesystem::path full_path = boost::filesystem::current_path() / entity.first / std::to_string(id);
+  // Update file
+  if(!manager_->DeleteFile(full_path)){
+    LOG(warning) << "Failed to delete file: " << full_path;
+    return INTERNAL_SERVER_ERROR;
+  }
+
+  // update response body with deleted id
+  std::string body = "{\"id\": " + std::to_string(id) + "}\n\n";
+  boost::beast::ostream(res.body()) << body;
+
+  LOG(info) << "Successfully deleted file: " << full_path;
+
+  // Remove id from set
+  (*entity_to_id_)[entity.first].erase(id);
+  LOG(info) << "Successfully removed id  " << id << " from entity " << entity.first;
+  return OK; 
+  
+}
+
+StatusCode CrudHandler::HandleGet(http::response<http::dynamic_body>& res) { 
+  std::pair<std::string, int> entity = ParseTarget();
+  //If id is included retrive information for specific instance of entity
+  if(entity.second!=-1){
+    int id = entity.second;
+    // check if entity with given id does not exist return bad request error
+    if(entity_to_id_->find(entity.first)==entity_to_id_->end() || (*entity_to_id_)[entity.first].find(id)== (*entity_to_id_)[entity.first].end()){
+      LOG(warning) << "Entity: " << entity.first << " not found with id " << id;
+      return BAD_REQUEST;
+    }
+
+    boost::filesystem::path full_path = boost::filesystem::current_path() / entity.first / std::to_string(id);
+    
+    // read file contents into response body
+    if (!manager_->ReadFile(full_path, res)) {
+      LOG(warning) << "Failed to read file: " << full_path;
+      return INTERNAL_SERVER_ERROR;
+    }
+  }
+  // if id is not included, returns JSON list of valid IDs for the given entity
+  else{
+
+    // if entity doesn't exist or return the empty list
+    if(entity_to_id_->find(entity.first)==entity_to_id_->end()){
+      boost::beast::ostream(res.body()) << "{[]}\n\n";
+      return OK;
+    }
+
+    //read in filenames from entity directory
+    boost::filesystem::path entity_dir = boost::filesystem::current_path() / entity.first;
+    std::vector<std::string> file_names;
+    if (!manager_->ListFiles(entity_dir, file_names)) {
+      LOG(warning) << "Failed to list files from " << entity_dir;
+      return INTERNAL_SERVER_ERROR;
+    }
+
+    //create JSON output
+    std::string body = "{[";
+    for (std::string file : file_names){
+      body += file + ", ";
+    }
+    //remove trailing comma if it exists
+    if(body.length()>2)
+      body.erase(body.length() - 2, 2);
+    body += "]}\n\n";
+
+    // update response body
+    boost::beast::ostream(res.body()) << body;
+  }
+
+  return OK; 
+}
 
 StatusCode CrudHandler::HandleRequest(const http::request<http::string_body> req,
                                       http::response<http::dynamic_body>& res) {
@@ -175,7 +262,7 @@ StatusCode CrudHandler::HandleRequest(const http::request<http::string_body> req
   res.result(status_code_);
   // boost::beast::ostream(res.body()) << "testing crud handler setup";
   res.prepare_payload();
-  res.set(boost::beast::http::field::content_type, "text/plain");
+  res.set(boost::beast::http::field::content_type, "application/json");
 
   return status_code_;
 }
