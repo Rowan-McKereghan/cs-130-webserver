ab306aa4948f4c735db3f0577da7610d0e5c1d92
ryan <ryand3031@g.ucla.edu>
2023-05-22 00:27:59 -0700

    Add implementations for Get and Delete requests - Add implementation for delete request for deleting instances of an entity 	Added a basic test for delete request - Add implementation for Get request 	- Handle Get request with ID for retrieving the information from a specific entity file 	- Handle Get request without ID for retrieving list of files in a given entity directory 		- returns empty list if entity does not exists 	- Added basic tests for Get functionality
    Change-Id: I3797ac1125e2393e9e1da30572e301e52a94212f

--- src/crud_handler_factory.cc
+++ src/crud_handler_factory.cc
@@ -8,7 +8,5 @@ CrudHandlerFactory::CrudHandlerFactory(NginxConfig* config) : config_(config) {}
 CrudHandler* CrudHandlerFactory::CreateHandler(std::string& file_path) {
   CrudFileSystemManager* manager = new CrudFileSystemManager;
   std::string complete_file_path = GetCompleteFilePath(config_, file_path);
-  std::string entity = "shoes";
- 
   return new CrudHandler(complete_file_path, entity_to_id_, manager);
 }
