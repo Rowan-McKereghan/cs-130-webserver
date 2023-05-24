415eafd2b85e698108de90a5673cc91ee7af006d
Kaashif Mohsin <kaashif01@g.ucla.edu>
2023-05-24 02:10:31 +0000

    Adding Integration tests for PUT and DELETE
    Change-Id: I4c25ce18ab96a909fb197a5c1543cc4114a65971

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -166,7 +166,7 @@ fi
 
 # -----------------
 
-
+#POST
 # CRUD Handler Valid Create action
 # Creates an entity and makes sure it's present
 
@@ -176,16 +176,16 @@ SERVER_PORT=80
 TIMEOUT=0.5
 
 #rm -rf ../crud/test/1
-rm -rf ../ctrl-c-ctrl-v/crud/test
+rm -rf ../ctrl-c-ctrl-v/crud/integration_test
 #rm -rf ../api/test
 #rm -rf entitydata??
 sleep 1.0
 
 # Curl server and create
-timeout $TIMEOUT curl -s -i -X POST -H "Host:" -H "User-Agent:" $SERVER_IP:$SERVER_PORT/api/test -d '{"a":111}'
+timeout $TIMEOUT curl -s -i -X POST -H "Host:" -H "User-Agent:" $SERVER_IP:$SERVER_PORT/api/integration_test -d '{"a":111}'
 
 # Check if file exists or not
-FILE=../ctrl-c-ctrl-v/crud/test/1
+FILE=../ctrl-c-ctrl-v/crud/integration_test/1
 
 test -f "$FILE";
 RESULT=$?
@@ -202,47 +202,129 @@ else
 fi
 
 
+# -----------------
 
+# -----------------
+
+#GET
 # CRUD Handler Valid Retrieve action
-# Creates an entity and makes sure it's present
+# Retrieves an entity and makes sure it's correct
+
+
 
 # Curl server and get list
-timeout $TIMEOUT curl -s -i -X GET -H "Host:" -H "User-Agent:" $SERVER_IP:$SERVER_PORT/api/test/1 > integration_test_GET1_result
+timeout $TIMEOUT curl -s -i -X GET -H "Host:" -H "User-Agent:" $SERVER_IP:$SERVER_PORT/api/integration_test/1 > integration_test_GET1_result
 
-# Check if file exists or not
-fname=tests/IntegrationDiffs/integration_test_GET1_exp
+# assign result to a variable
 
-#diff tests/IntegrationDiffs/integration_test_GET1_exp integration_test_GET1_result
-diff "integration_test_GET1_result" "$fname" | grep "^<" | wc -l
+fname=integration_test_GET1_result
+
+
+#ignore date as that is a temporary metric and compare the rest to confirm info is accurate
+diff -b -I '^Date*' "tests/IntegrationDiffs/integration_test_GET1_exp" "$fname"
 
 RESULT=$?
 
-DIFFLINE= diff "integration_test_GET1_result" "$fname" | grep "^<" 
+
+if [[ "$RESULT" -eq 1 ]]
+then 
+    printf "GET: failure\n"
+    (( ++numberOfTests ))
+    finalExit=1
+else
+    printf "GET: success\n"
+    (( ++numberOfTests ))
+    (( ++numberOfSucceededTests ))
+fi
+
+
+
+
+
+# -----------------
+
+# -----------------
+
+
+
+#PUT
+# CRUD Handler Valid Update action
+
+# Curl server and update entity
+timeout $TIMEOUT curl -s -i -d '{"a":123}' -X PUT -H "Host:" -H "User-Agent:" $SERVER_IP:$SERVER_PORT/api/integration_test/1
+
+# Curl server and get updated entity
+timeout $TIMEOUT curl -s -i -X GET -H "Host:" -H "User-Agent:" $SERVER_IP:$SERVER_PORT/api/integration_test/1 > integration_test_PUT_result
+
+# Check if file exists or not
+RESULT=diff tests/IntegrationDiffs/integration_test_PUT_exp integration_test_PUT_result | grep "^<" | wc -l
+
+
+
+fname=tests/IntegrationDiffs/integration_test_PUT_exp
+
+DIFFLINE= diff "integration_test_PUT_result" "$fname" | grep "^<" 
 
 DIFFWORD= $DIFFLINE | head -n1 | cut -d " " -f1
+#isolates the first word of the different line to check if it's the Date line
 
-rm integration_test_GET1_result
+rm integration_test_PUT_result
 
 if ([ $RESULT -eq 1 ] && [ $DIFFWORD -eq Date:]);
 then
-    #TEST_GET_SUCCESS=1
+    #TEST_PUT_SUCCESS=1
     (( ++numberOfTests ))
     (( ++numberOfSucceededTests ))
-    echo "Test GET1 Success"
+    echo "Test PUT Success"
 elif [ $RESULT -eq 0 ];
 then
     #TEST_GET_SUCCESS=1
     (( ++numberOfTests ))
     (( ++numberOfSucceededTests ))
-    echo "Test GET1 Success"
-else 
-    #SUCCESS = 0
-    echo "Test GET1 Failure"
+    echo "Test PUT Success"
+else
+    #TEST_PUT_SUCCESS=0
+    echo "Test PUT Failure"
     (( ++numberOfTests ))
     finalExit=1
 fi
 
 
+# -----------------
+
+# -----------------
+
+
+#DELETE
+# CRUD Handler Valid remove action
+
+
+# Curl server and delete
+timeout $TIMEOUT curl -s -i -X DELETE -H "Host:" -H "User-Agent:" $SERVER_IP:$SERVER_PORT/api/integration_test/1
+
+# Check if file exists or not
+FILE=.../ctrl-c-ctrl-v/crud/integration_test/1
+
+test -f "$FILE";
+RESULT=$?
+
+if [ $RESULT -ne 0 ];
+then
+    #TEST_DELETE_SUCCESS=1
+    (( ++numberOfTests ))
+    (( ++numberOfSucceededTests ))
+    echo "$FILE was deleted."
+else 
+    #TEST_DELETE_SUCCESS=0
+    echo "$FILE was not deleted."
+    (( ++numberOfTests ))
+    finalExit=1
+fi
+
+
+# -----------------
+
+# -----------------
 
 
 printf "Tests Passed: $numberOfSucceededTests/$numberOfTests\n"
@@ -254,7 +336,6 @@ exit $finalExit
 
 
 #diff btwn expected curl and curl result (tests/IntegrationDiffs/expected_curl_basic.txt)
-
 
 
 
