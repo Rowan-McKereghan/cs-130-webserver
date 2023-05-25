785d6e51cd72d9e08c4b2aac9548264af2e667f5
Rowan McKereghan <romckereghan@g.ucla.edu>
2023-05-25 02:26:18 +0000

    health handler added for basic monitoring
    Change-Id: I1bf88a50138f1ffd0e1cbd2c68cb1fcac25784ab

--- tests/request/health_handler_test.cc
+++ tests/request/health_handler_test.cc
@@ -1,24 +1,21 @@
-#include "echo_handler.h"
+#include "health_handler.h"
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 
 #include "gtest/gtest.h"
 
-TEST(EchoHandler, BasicSuccessEchoTest) {
+TEST(HealthHandler, BasicSuccessHealthTest) {
   std::string req_data = "Sample data to be echoed";
   boost::beast::http::request<boost::beast::http::string_body> req{boost::beast::http::verb::get,  // GET
-                                                                   "/echo",                        // URI
+                                                                   "/health",                        // URI
                                                                    11};                            // HTTP 1.1
   req.body() = req_data;
   boost::beast::http::response<boost::beast::http::dynamic_body> res;
-  EchoHandler handler;
+  HealthHandler handler;
   handler.HandleRequest(req, res);
 
-  boost::beast::http::dynamic_body::value_type req_body;
-  boost::beast::ostream(req_body) << req;
-
-  EXPECT_EQ(boost::beast::buffers_to_string(res.body().data()), boost::beast::buffers_to_string(req_body.data()));
+  EXPECT_EQ(boost::beast::buffers_to_string(res.body().data()), "OK");
   EXPECT_EQ(res.version(), 11);
   EXPECT_EQ(res.result_int(), OK);
 }
