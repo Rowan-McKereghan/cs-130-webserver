8e1069b4fe79387850f3859a0fd2b36faf6ca5db
Rowan McKereghan <romckereghan@g.ucla.edu>
2023-05-27 00:32:18 +0000

    serve 400 on nonsense
    Change-Id: I8b049c3d58c3ebce9240c3f8646c7ec19dc25432

--- src/health_handler.cc
+++ src/health_handler.cc
@@ -8,12 +8,14 @@
 #include "logger.h"
 
 StatusCode HealthHandler::HandleRequest(const boost::beast::http::request<boost::beast::http::string_body> req,
-                                        boost::beast::http::response<boost::beast::http::dynamic_body>& res) {
-  res.result(boost::beast::http::status::ok);
-  res.version(req.version());
-  res.set(boost::beast::http::field::server, "webserver");
-  boost::beast::ostream(res.body()) << "OK";
-  res.set(boost::beast::http::field::content_type, "text/plain");
-  res.prepare_payload();
-  return OK;
+                                      boost::beast::http::response<boost::beast::http::dynamic_body>& res) {
+    
+    res.result(boost::beast::http::status::ok);
+    res.version(req.version());
+    res.set(boost::beast::http::field::server, "webserver");
+    boost::beast::ostream(res.body()) << "OK";
+    res.set(boost::beast::http::field::content_type, "text/plain");
+    res.prepare_payload();
+    return OK;
+  
 }
